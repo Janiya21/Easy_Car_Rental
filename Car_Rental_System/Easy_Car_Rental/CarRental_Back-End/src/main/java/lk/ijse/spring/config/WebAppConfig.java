89062f38d4cf7a1c20b35688808a9de6454f5297
JanithSandaru <janithsandaru999@gmail.com>
2022-07-14 10:20:58 +0530

    ....

--- Car_Rental_System/Easy_Car_Rental/CarRental_Back-End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Car_Rental_System/Easy_Car_Rental/CarRental_Back-End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -8,12 +8,14 @@ import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.spring")
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
@@ -23,4 +25,10 @@ public class WebAppConfig {
     public MultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**").addResourceLocations("/uploads/");
+    }
+
 }

acbf3e9d480d6df4007c4894c1ba4b96b0424df8
JanithSandaru <janithsandaru999@gmail.com>
2022-07-16 11:08:05 +0530

    admin controller js added

--- Car_Rental_System/Easy_Car_Rental/CarRental_Front-End/controller/AdminController.js
+++ Car_Rental_System/Easy_Car_Rental/CarRental_Front-End/controller/AdminController.js
@@ -1,15 +1,32 @@
-let baseUrl = "http://localhost:8081/easyRents/";
-
 $("#loginBtn").click(function (){
 
 });
 
+loginCheck();
+
 function loginCheck(){
+
+    let username = $("#txtAdminUserName").val();
+    let password = $("#txtAdminPassword").val();
+
     $.ajax({
-        url: "http://localhost:8081/easyRents/api/v1/admin",
+        url: baseUrl + "api/v1/admin",
         method: "GET",
         success: function (resp) {
+            let data = resp.data;
+            for (let ad of data) {
+                console.log(ad.userName , ad.password);
+                if(username === ad.userName){
+                    if(password === ad.password){
 
+                    }else{
+                        alert("Wrong Password !!");
+                    }
+                }
+            }
+        },
+        error: function (err) {
+            console.log(err);
         }
     });
 }
